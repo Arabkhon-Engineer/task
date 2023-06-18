7f2493d618b47f2b72271bfbf4e4be9309c6fbd3
Arabkhon-enginner <arabkhon2322@gmail.com>
2023-06-18 12:46:58 +0300

    make model Application

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,5 @@ class User extends Authenticatable
     ];
 
     public function role(){return $this->belongsTo(Role::class);}
+    public function applications(){return $this->hasMany(Application::class);}
 }
